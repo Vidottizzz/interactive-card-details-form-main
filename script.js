cd7560c44fe5d6adf70ed05576ad68aaacc30563
Vidotti <vidottizzz@gmail.com>
2023-03-21 14:44:05 -0300

    javascript input appearing in the div (front)

--- script.js
+++ script.js
@@ -5,14 +5,34 @@ let input = document.querySelectorAll(".first-input");
 let btn = document.querySelector("#submit-answ");
 let errMsg = document.querySelector(".err-msg");
 let errMsg1 = document.querySelector(".err-msg1");
+let errMsg11 = document.querySelector(".err-msg11");
 let errMsg2 = document.querySelector(".err-msg2");
 let errMsg3 = document.querySelector(".err-msg3");
 let form = document.querySelector("#form");
+let cardNameInput = document.querySelector("#cardname");
+let cardNameDiv = document.querySelector("#card-name");
+let cardNumInput = document.querySelector("#cardnum");
+let cardNumDiv = document.querySelector("#card-num");
+let cardMonthInput = document.querySelector("#expMonth");
+let cardMonthDiv = document.querySelector("#card-date");
+let cardYearInput = document.querySelector("#expYear");
+let cardYearDiv = document.querySelector("#card-date1");
+let cardCVCDiv = document.querySelector("#card2p");
+let cardCVCInput = document.querySelector("#cvc-input");
 
 function handleForm(event) {
   event.preventDefault();
 }
 form.addEventListener("submit", handleForm);
+
+function myFunction()
+{
+    cardNameDiv.innerHTML =  cardNameInput.value;
+    cardNumDiv.innerHTML =  cardNumInput.value;
+    cardMonthDiv.innerHTML =  cardMonthInput.value;
+    cardYearDiv.innerHTML =   `/${cardYearInput.value}`;
+    cardCVCDiv.innerHTML =   cardCVC  Input.value;
+}
 
 function validate() {
   let name = document.forms["Form"]["cardname"].value;
@@ -21,31 +41,30 @@ function validate() {
   let exp1 = document.forms["Form"]["expdate1"].value;
   let cvc = document.forms["Form"]["cvc"].value;
 
-  //   if (!num.match(/^\d+/)) {
-
-  //     errMsg2.classList.remove("hide");
-
-  //   }
-
-  if (name == "") {
-    errMsg.classList.remove("hide");
-    document.forms["Form"]["cardname"].classList.add("border-red");
-
-  } else {
-    errMsg.classList.add("hide");
-    document.forms["Form"]["cardname"].classList.remove("border-red");
-  }
-
-  // deixar o distanciamento do err msg para card number
+    
+    var letters = /^[A-Za-z0-9\s]+$/;
+    var result = letters.test(num);
+  
+    if (name == "") {
+      errMsg.classList.remove("hide");
+      document.forms["Form"]["cardname"].classList.add("border-red");
+    } else {
+      errMsg.classList.add("hide");
+      document.forms["Form"]["cardname"].classList.remove("border-red");
+    }
+    
+    // deixar o distanciamento do err msg para card number
 
   if (num == "") {
     errMsg1.classList.remove("hide");
+
     document.forms["Form"]["cardnum"].classList.add("border-red");
   } else {
     errMsg1.classList.add("hide");
+
     document.forms["Form"]["cardnum"].classList.remove("border-red");
-    
   }
+
   if (cvc == "") {
     errMsg3.classList.remove("hide");
     document.forms["Form"]["cvc"].classList.add("border-red");
@@ -71,3 +90,4 @@ function validate() {
     document.forms["Form"]["expdate1"].classList.remove("border-red");
   }
 }
+
